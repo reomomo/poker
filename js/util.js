0e7f28bf6e6ddd6e327b7d79391765db3b21814a
reomomo <reomomo31672@gmail.com>
2023-08-11 12:51:19 +0000

    poker作成開始

---
+++ js/util.js
@@ -0,0 +1,35 @@
+/**
+* Utilクラス
+*/
+
+export default class Util {
+/**
+* 指定した時間だけ待つ（未指定の場合は1秒）
+*/
+
+  static sleep = (wait = 1000) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(), wait);
+    });
+  };
+
+  /**
+  * イベントハンドラを追加する
+  */
+
+  static addEventListener = (selector, event, handler) => {
+    document.querySelectorAll(selector).forEach((e) => e.addEventListener(event, handler));
+  };
+
+  /**
+  * 数値を合計する
+  */
+
+  static sum = (...numbers) => {
+    let sum = 0;
+    numbers.forEach((e) => {
+      sum += e;
+    });
+    return sum;
+  };
+}
