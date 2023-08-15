45e4bebfca287e68e307c7b40bedef2058ceaf95
reomomo <reomomo31672@gmail.com>
2023-08-15 13:06:15 +0000

    poker完成

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ import Player from "./player.js";
 import Com from "./com.js";
 import Card from "./card.js";
 import Util from "./util.js";
+import Pair from "./pair.js";
+
  /**
  * Game クラス
  */
 export default class Game {
+
   /**
    * プロパティ
    */
@@ -26,6 +29,7 @@ export default class Game {
     // イベントハンドラを登録する
     this.#setupEvents();
   }
+
   /**
    * イベントハンドラを登録する
    */
@@ -84,6 +88,7 @@ export default class Game {
       [this.#cards[j], this.#cards[k]] = [this.#cards[k], this.#cards[j]];
     });
   }
+
   /**
    * 山札のカードをプレイヤーに配る
    */
@@ -107,6 +112,7 @@ export default class Game {
     if (this.#isRunning) {
       document.querySelector("#replay").setAttribute("disabled", true);
       document.querySelector("#draw").removeAttribute("disabled");
+    } else {
       document.querySelector("#replay").removeAttribute("disabled");
       document.querySelector("#draw").setAttribute("disabled", true);
     }
@@ -116,17 +122,88 @@ export default class Game {
    * 手札のクリックイベントハンドラ
    */
   #onClickCard(event) {
-  }
+    console.log("test")
+    // ゲーム実行中のみクリックを受け付ける
+    if (this.#isRunning) {
+      // プレイヤーにカードを選択する
+      this.#you.selectCard(event.target);
+    }
+  };
 
   /**
    * Drawボタンのクリックイベントハンドラ
    */
-  #onDraw(event) {
+  async #onDraw(event) {
+    // プレイヤーがカードを更新する
+    /**
+     * 山札の一番上から1枚取り出す
+     * 取り出した1枚をプレイヤークラスのdrawCardメソッドに渡す
+     * drawCardメソッドが返すカードを山札の一番下に戻す
+     */
+    this.#you.selectedNodes.forEach(() => {
+      this.#cards.unshift(this.#you.drawCard(this.#cards.pop()));
+    });
+    // 画面の描画を更新する
+    this.#updateView();
+    // ゲーム実行状態を更新
+    this.#isRunning = false;
+    // 1秒待つ
+    await Util.sleep(); // デフォルトは1秒
+    // 相手が交換するカードを選ぶ
+    this.#com.selectCard();
+    // 1秒待つ
+    await Util.sleep(); // デフォルトは1秒
+    // 相手がカードを交換する
+    this.#com.selectedNodes.forEach(() => {
+      this.#cards.unshift(this.#com.drawCard(this.#cards.pop()));
+    });
+    // 画面の描画を更新する
+    this.#updateView();
+    // 1秒待つ
+    await Util.sleep(); // デフォルトは1秒
+    // 勝敗を判定する
+    this.#judgement();
   }
+  /**
+   * 勝敗を判定する
+   */
+  #judgement() {
+    // 役の判定を行う
+    const youResult = Pair.judge(this.#you.cards);
+    const comResult = Pair.judge(this.#com.cards);
+    // 勝敗のメッセージ
+    let message = `(YOU)${youResult.hand}vs(COM)${comResult.hand}\n`;
+    // 勝敗の判定
+    if (youResult.strength < comResult.strength) {
+      // 相手（Com）の勝ち
+      message += `あなたの負けです`;
+    } else if (youResult.strength > comResult.strength) {
+      // プレイヤーの勝ち
+      message += `あなたの勝ちです`;
+    } else {
+      // 役が同じなのでランクで比較する
+      if (youResult.rank < comResult.rank) {
+        // 相手（Com）の勝ち
+        message += `あなたの負けです`;
+      } else if (youResult.rank > comResult.rank) {
+        // プレイヤーの勝ち
+        message += `あなたの勝ちです`;
+      } else {
+        // 引き分け
+        message += `引き分けです`;
+      }
+    }
+    // メッセージを表示する
+    alert(message);
+  }
+
+
 
   /**
    * Replayボタンのクリックイベントハンドラ
    */
   #onReplay(event) {
+    // ゲームの状態を初期化する
+    this.#initialize();
   }
 }
